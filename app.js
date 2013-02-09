b03fc32f815a5d0cebe292e0dde3fe22ce710454
joseph <joseph.piron@gmail.com>
2013-02-09 22:31:01 +0100

    getting interpreted values !

--- app.js
+++ app.js
@@ -1,56 +1,56 @@
 var express = require('express'),
-	app = express(),
-	fs = require('fs'),
-	util = require('util'),
-	routes = require('./routes'),
-	user = require('./routes/user'),
-	server = require('http').createServer(app),
-	path = require('path'),
-	io = require('socket.io').listen(server, {log: false}),
-	stylus = require('stylus');
+    app = express(),
+    fs = require('fs'),
+    util = require('util'),
+    routes = require('./routes'),
+    user = require('./routes/user'),
+    server = require('http').createServer(app),
+    path = require('path'),
+    io = require('socket.io').listen(server, {log: false}),
+    stylus = require('stylus');
 
 server.listen(app.get('port') || 8000, function(){
-	console.log("Express server listening on port " + app.get('port'));
+    console.log("Express server listening on port " + app.get('port'));
 });
 
 app.configure(function(){
-	app.set('views', __dirname + '/views');
-	app.set('view engine', 'jade');
-	app.use(express.favicon());
-	app.use(express.logger('dev'));
-	app.use(express.bodyParser());
-	app.use(express.methodOverride());
-	app.use(app.router);
-	app.use(stylus.middleware({
-		src: path.join(__dirname, 'public'), compress:true
-	}));
-	app.use(express.static(path.join(__dirname, 'public')));
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(stylus.middleware({
+        src: path.join(__dirname, 'public'), compress:true
+    }));
+    app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.configure('development', function(){
-	app.use(express.errorHandler());
+    app.use(express.errorHandler());
 });
 
 app.get('/', routes.index);
 app.get('/users', user.list);
 
 var config = require('./config.json'),
-	swap = require('./swap'),
-	SerialModem = require('./serialModem'),
-	manager = require('./manager.js'),
-	log4js = require('log4js');
+    swap = require('./swap'),
+    SerialModem = require('./serialModem'),
+    manager = require('./manager.js'),
+    log4js = require('log4js');
 
 log4js.setGlobalLogLevel(log4js.levels.INFO);
 var swapManager = undefined, 
-	serial = new SerialModem(config);
+    serial = new SerialModem(config);
 
 serial.on('started', function(){
-	swapManager = new manager(serial, config);
-	swapManager.on("newMote", function(mote){
-		io.sockets.emit('newMote', mote)
-	});
+    swapManager = new manager(serial, config);
+    swapManager.on("newMote", function(mote){
+        io.sockets.emit('newMote', mote)
+    });
 });
 
 serial.on("data", function(packet){
-	io.sockets.emit('swapPacket', packet);
+    io.sockets.emit('swapPacket', packet);
 });
--- swap.js
+++ swap.js
@@ -1,63 +1,72 @@
 var logger = require('log4js').getLogger(__filename.split('/').pop(-1).split('.')[0]);
 
 module.exports = {
-	CCPacket: function (strPacket){
-		if ((strPacket.length)%2 != 0){
-			logger.error("Packet length must be even: " + strPacket.length);
-			return;
-		}
-		this.RSSI = parseInt(strPacket.slice(1,3), 16);
-		this.LQI = parseInt(strPacket.slice(3,5), 16);
-		this.data = [];
-		for(i = 6; i< strPacket.length; i+=2)
-			this.data.push(parseInt(strPacket.slice(i,i+2), 16));	
-	},
+    CCPacket: function (strPacket){
+        if ((strPacket.length)%2 != 0){
+            logger.error("Packet length must be even: " + strPacket.length);
+            return;
+        }
+        this.RSSI = parseInt(strPacket.slice(1,3), 16);
+        this.LQI = parseInt(strPacket.slice(3,5), 16);
+        this.data = [];
+        for(i = 6; i< strPacket.length; i+=2)
+            this.data.push(parseInt(strPacket.slice(i,i+2), 16));   
+    },
 
-	SwapPacket: function (ccPacket) {			
-		this.RSSI = ccPacket.RSSI;
-		this.LQI = ccPacket.LQI;
-		this.dest = ccPacket.data[0];
-		this.source = ccPacket.data[1];
-		this.hop = ccPacket.data[2] >> 4 && 0x0F;
-		this.security = ccPacket.data[2] && 0x0F;
-		this.nonce = ccPacket.data[3];
-		this.func = ccPacket.data[4];
-		this.regAddress = ccPacket.data[5];
-		this.regId = ccPacket.data[6];
-		this.value = ccPacket.data.slice(7,ccPacket.data.length);
-	},
+    SwapPacket: function (ccPacket) {           
+        this.RSSI = ccPacket.RSSI;
+        this.LQI = ccPacket.LQI;
+        this.dest = ccPacket.data[0];
+        this.source = ccPacket.data[1];
+        this.hop = ccPacket.data[2] >> 4 && 0x0F;
+        this.security = ccPacket.data[2] && 0x0F;
+        this.nonce = ccPacket.data[3];
+        this.func = ccPacket.data[4];
+        this.regAddress = ccPacket.data[5];
+        this.regId = ccPacket.data[6];
+        this.value = ccPacket.data.slice(7,ccPacket.data.length);
+    },
 
-	SwapMote: function(address, network, channel, security, nonce){		 
-		// Standards registers
-		this.productCode= null,
-		this.hardwareVersion= null,
-		this.firmwareVersion= null,
-		this.state= null,
-		this.channel= channel,
-		this.security= security,
-		this.password= null,
-		this.nonce= nonce,
-		this.network= network,
-		this.address= address,
-		this.txInterval= null
-		this.lastStatusTime = null
-	},
+    SwapMote: function(address, network, channel, security, nonce){      
+        // Standards registers
+        this.productCode= undefined,
+        this.hardwareVersion= undefined,
+        this.firmwareVersion= undefined,
+        this.state= undefined,
+        this.channel= channel,
+        this.security= security,
+        this.password= undefined,
+        this.nonce= nonce,
+        this.network= network,
+        this.address= address,
+        this.txInterval= undefined
+        this.lastStatusTime = undefined
+    },
 
-	Functions: {
-		STATUS: 0,
-		QUERY: 1,
-		COMMAND: 2
-	},
+    Endpoint: function(id){
+        this.id = id,
+        this.name = undefined,
+        this.location = undefined,
+        this.vale = undefined,
+        this.unit = undefined,
+        this.dir = undefined
+    },
 
-	SwapStates: {
-		RESTART: {level: 0, str: "Restart"},
-		RXON: {level: 1, str: "Radio On"},
-		RXOFF: {level: 2, str: "Radio Off"},
-		SYNC: {level: 3, str: "Sync mode"},
-		LOWBAT: {level: 4, str: "Low battery"},
+    Functions: {
+        STATUS: 0,
+        QUERY: 1,
+        COMMAND: 2
+    },
 
-		get: function(val){ 
-			return [this.RESTART, this.RXON, this.RXOFF, this.SYNC, this.LOWBAT][val]; 
-		}
-	},
+    SwapStates: {
+        RESTART: {level: 0, str: "Restart"},
+        RXON: {level: 1, str: "Radio On"},
+        RXOFF: {level: 2, str: "Radio Off"},
+        SYNC: {level: 3, str: "Sync mode"},
+        LOWBAT: {level: 4, str: "Low battery"},
+
+        get: function(val){ 
+            return [this.RESTART, this.RXON, this.RXOFF, this.SYNC, this.LOWBAT][val]; 
+        }
+    },
 };
